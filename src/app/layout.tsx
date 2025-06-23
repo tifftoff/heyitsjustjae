c426d1df0c35f06dc15c4c45f7b9427f9ee47141
tifftoff <tiffanyx@google.com>
2025-06-23 23:47:27 +0800

    link chip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,32 @@
-import type { Metadata } from "next";
-import { Doto } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Doto } from 'next/font/google';
+import './globals.css';
 
-import { Footer } from "@/components/Footer";
+import { Footer } from '@/components/Footer';
 
 const doto = Doto({
-  variable: "--font-doto",
-  subsets: ["latin"],
+  variable: '--font-doto',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "heyitsjustjae - official website",
-  description: "heyitsjustjae - official website",
+  title: 'heyitsjustjae - official website',
+  description: 'heyitsjustjae - official website',
+  openGraph: {
+    title: 'heyitsjustjae - official website',
+    description: 'heyitsjustjae - official website',
+    url: 'https://heyitsjustjae.vercel.app', 
+    siteName: 'heyitsjustjae - official website',
+    images: [
+      {
+        url: '/icon.svg',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  }
 };
 
 export default function RootLayout({
@@ -20,9 +35,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`flex flex-col min-h-[100dvh] ${doto.variable} antialiased`} >
-        <main className="flex flex-grow">
+    <html lang='en'>
+      <body className={'flex flex-col min-h-[100dvh] ${doto.variable} antialiased'} >
+        <main className='flex flex-grow'>
           {children}
         </main>
         <Footer />
