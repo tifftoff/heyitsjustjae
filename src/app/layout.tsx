80fe2d6d5da926ead1f798700f9307ec961866c6
tifftoff <tiffanyxiang7@gmail.com>
2025-12-23 13:28:52 -0500

    add meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <meta name="google-site-verification" content="FRHjFBevsa0-4BRugQJ0lADRXYCT_1qLbW7DWXkaiBg" />
       <body className="flex flex-col min-h-[100dvh] antialiased" >
         <main className='flex flex-grow min-h-[100dvh] justify-center items-center w-full'>
           {children}
