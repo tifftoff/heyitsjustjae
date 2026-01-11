0c960cc69e9bbbc6234c8757411a3b895d3c7a63
tifftoff <tiffanyxiang7@gmail.com>
2026-01-11 15:11:03 -0500

    mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ const Title = () => {
 
 export default function Home() {
   return (
-    <main className='flex justify-center items-center w-full'>
+    <main className='flex justify-center items-center w-full pt-40 md:pt-0'>
       {/* Icon bar */}
       <div className='absolute top-8 left-8 flex flex-col gap-4 md:gap-6 text-[clamp(2rem,4vw,2.5rem)] text-center'>
         {socials.map((link) => (
