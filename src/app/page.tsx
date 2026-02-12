9a0ab53103fa3ced9c98fc0b0be70defb29937e2
tifftoff <tiffanyxiang7@gmail.com>
2026-02-12 13:28:21 -0500

    revamp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,47 @@
 'use client';
 
+import { Rubik_Glitch, Work_Sans } from 'next/font/google';
 import { TypeAnimation } from 'react-type-animation';
 import { FaXTwitter, FaInstagram, FaSoundcloud, FaTiktok, FaSpotify } from 'react-icons/fa6';
 import { useState } from 'react';
 
+const rubik = Rubik_Glitch({
+  weight: ['400'],
+  variable: '--font-rubik',
+});
+
+const work = Work_Sans({
+  weight: ['700'],
+  variable: '--font-work',
+});
+
 const socials = [
+  { href: 'https://x.com/heyitsjustjae', label: 'X', icon: <FaXTwitter /> },
+  { href: 'https://open.spotify.com/user/jamesp12349?si=37b55767bf6d4216', label: 'Spotify', icon: <FaSpotify /> },
+  { href: 'https://soundcloud.com/heyitsjustjae', label: 'Soundcloud', icon: <FaSoundcloud /> },
   { href: 'https://www.instagram.com/heyitsjustjae', label: 'Instagram', icon: <FaInstagram /> },
   { href: 'https://tiktok.com/@heyitsjustjae', label: 'TikTok', icon: <FaTiktok /> },
-  { href: 'https://soundcloud.com/heyitsjustjae', label: 'Soundcloud', icon: <FaSoundcloud /> },
-  { href: 'https://open.spotify.com/user/jamesp12349?si=37b55767bf6d4216', label: 'Spotify', icon: <FaSpotify /> },
-  { href: 'https://x.com/heyitsjustjae', label: 'X', icon: <FaXTwitter /> },
 ];
+
+const Socials = () => {
+  return (
+    <div className='flex justify-center gap-5 text-[clamp(1rem,1.75vw,2rem)]'>
+        {socials.map((link) => (
+          <a
+            key={link.href} href={link.href} aria-label={link.label}
+            className='hover:text-neutral-400 transition-colors duration-300 ease-in-out'
+          >
+            {link.icon}
+          </a>
+        ))}
+      </div>
+  );
+};
 
 const Title = () => {
   const [isDone, setIsDone] = useState(false);
-
   return (
-    <h1 className='font-[family-name:var(--font-ocr)] text-[clamp(1rem,10vw,5rem)]'>
+    <h1 className={`font-[family-name:var(--font-rubik)] text-[clamp(1rem,6vw,6rem)]`}>
       {!isDone ? (
         <TypeAnimation
           sequence={[
@@ -40,39 +65,48 @@ const Title = () => {
   );
 };
 
+const Nav = () => {
+  const scrollTo = (id: string) => {
+    document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  return (
+    <nav className={`absolute font-[family-name:var(--font-work)] bottom-12 w-full flex justify-center gap-20 md:gap-120 text-xl md:text-4xl`}>
+      <button 
+        onClick={() => scrollTo('music')}
+        className="hover:text-neutral-400 transition-colors duration-200 ease-in-out cursor-pointer uppercase tracking-widest"
+      >
+        Music
+      </button>
+      <button 
+        onClick={() => scrollTo('shows')}
+        className="hover:text-neutral-400 transition-colors duration-200 ease-in-out cursor-pointer uppercase tracking-widest"
+      >
+        Shows
+      </button>
+    </nav>
+  );
+};
+
+// const Music = () => { return };
+// const Shows = () => { return };
+
 export default function Home() {
   return (
-    <main className='flex justify-center items-center w-full pt-40 md:pt-0'>
+    <main className={`${rubik.variable} ${work.variable} flex justify-center items-center w-full pt-40 md:pt-0`}>
       {/* Icon bar */}
-      <div className='absolute top-8 left-8 flex flex-col gap-4 md:gap-6 text-[clamp(2rem,4vw,2.5rem)] text-center'>
-        {socials.map((link) => (
-          <a
-            key={link.href} href={link.href} aria-label={link.label}
-            className='hover:text-neutral-400 transition-colors duration-300 ease-in-out'
-          >
-            {link.icon}
-          </a>
-        ))}
+      <div className="absolute top-6 w-full">
+        <Socials />
       </div>
 
       {/* Title */}
       <Title />
 
-      {/* Email */}
-      <div className="absolute md:bottom-8 md:right-8 md:top-auto top-6 right-8 font-[family-name:var(--font-ocr)] text-xl md:text-3xl">
-        <a 
-          href="mailto:jae.pena@outlook.com" 
-          className="group relative flex items-center justify-end"
-        >
-          <span className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 mr-2 pointer-events-none">
-            jae.pena@outlook.com
-          </span>
-          
-          <span className="group-hover:text-gray-500 transition-colors duration-300">
-            +contact
-          </span>
-        </a>
-      </div>
+      <Nav />
+
+      {/* <Music /> */}
+      {/* <Shows /> */}
+      
     </main>
   );
 }
